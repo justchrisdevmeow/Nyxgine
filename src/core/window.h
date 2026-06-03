2be1b255a5fd0383b766bc2d90a724e2d39868cc
notchwis <kodenothing@gmail.com>
2026-06-03 10:25:12 -0500

    Add window.h for window management functions

---
+++ src/core/window.h
@@ -0,0 +1,19 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <GLFW/glfw3.h>
+
+typedef struct {
+    GLFWwindow* handle;
+    int width, height;
+    const char* title;
+} Window;
+
+Window* window_create(int width, int height, const char* title);
+void window_destroy(Window* win);
+int window_should_close(Window* win);
+void window_swap_buffers(Window* win);
+void window_poll_events(void);
+float window_get_time(void);
+
+#endif
