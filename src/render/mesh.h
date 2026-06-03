8eb761100e8e9075907425bceabcf59ac8178ea7
notchwis <kodenothing@gmail.com>
2026-06-03 10:28:05 -0500

    Add mesh.h header for mesh data structures and functions

---
+++ src/render/mesh.h
@@ -0,0 +1,17 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <GL/glew.h>
+
+typedef struct {
+    unsigned int vao, vbo;
+    int vertex_count;
+} Mesh;
+
+Mesh* mesh_create_cube(void);
+Mesh* mesh_create_sphere(float radius, int rings, int sectors);
+Mesh* mesh_create_plane(float size);
+void mesh_draw(Mesh* m);
+void mesh_destroy(Mesh* m);
+
+#endif
